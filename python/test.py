6b952d6c6e37b199d661d114d7eaef5c22f48673
Michael Purser <michael.purser@outlook.be>
2019-03-16 13:17:51 +0100

    modified test.py or git testing, moved SQP.casadi to correct folder, updated .gitignore to also include npm file

--- python/test.py
+++ python/test.py
@@ -4,6 +4,8 @@ sys.path.append("/home/michael/Downloads/casadi/install-python")
 from casadi import *
 import numpy as np
 
+print('This is a test')
+
 SQP = Function.load("SQP.casadi")
 
 [x,u,T] = SQP(np.vstack((np.linspace(0,10,201),np.linspace(0,10,201),np.linspace(0,np.pi,201))),np.vstack((np.linspace(0,0.1,200),np.linspace(0,0.1,200))),70,np.hstack((20*np.ones((1000,1)),20*np.ones((1000,1)))),0.2,-0.2,0.2,-0.1,0.1,-1,1,2,0.2)
